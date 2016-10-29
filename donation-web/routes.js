f1ab2beec06161937c3f194cd6b1839cf5f53313
FlorianFusseder-Laptop <florianfueder@yahoo.de>
2016-10-29 16:27:49 +0200

    Finished lab 7s exercises

--- donation-web/routes.js
+++ donation-web/routes.js
@@ -7,13 +7,15 @@ module.exports = [
   { method: 'GET', path: '/', config: Accounts.main },
   { method: 'GET', path: '/signup', config: Accounts.signup },
   { method: 'GET', path: '/login', config: Accounts.login },
+  { method: 'POST', path: '/login', config: Accounts.authenticate },
+  { method: 'POST', path: '/register', config: Accounts.register },
   { method: 'GET', path: '/logout', config: Accounts.logout },
+  { method: 'GET', path: '/settings', config: Accounts.viewSettings },
+  { method: 'POST', path: '/settings', config: Accounts.updateSettings },
+
   { method: 'GET', path: '/home', config: Donations.home },
   { method: 'GET', path: '/report', config: Donations.report },
-
   { method: 'POST', path: '/donate', config: Donations.donate },
-  { method: 'POST', path: '/register', config: Accounts.register },
-  { method: 'POST', path: '/login', config: Accounts.authenticate },
 
   {
     method: 'GET',
